2be89944157f6f34d91a11ad8017f6a7a17cce5d
Your Name <you@example.com>
2021-11-09 17:11:30 +0800

    h

--- example/wifi_connect/wifi_connect.ino
+++ example/wifi_connect/wifi_connect.ino
@@ -1,9 +1,9 @@
 #include<wifi_connect_tool.h>
 void setup() {
-  EEPROM.begin(1024);//向系统申请1024kb ROM
-  for (int i = 0; i < 1024; i++)
-    EEPROM.write(i, 0);
-  EEPROM.commit();//执行写入ROM
+//  EEPROM.begin(1024);//向系统申请1024kb ROM
+//  for (int i = 0; i < 1024; i++)
+//    EEPROM.write(i, 0);
+//  EEPROM.commit();//执行写入ROM
   // put your setup code here, to run once:
   delay(100);
   load();
